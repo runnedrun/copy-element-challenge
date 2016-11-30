84348ce9e751a4c4632e4d2fb63af73aeafdef14
David Gaynor <runnedrun@gmail.com>
2016-12-01 00:15:38 +0800

    Classadoo autocommit

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // hints!
 // 1. find the element in index.html that you want to copy
-// 2.
+// 2. select it somehow (by class . or id #)
+// 3. google 'jquery copy element'
+// 4. copy the element and add the new ele it to the element list
 
 var elementsList = $("#elements-list")
 
